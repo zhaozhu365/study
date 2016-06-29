54b0b5496beb8ce4027c49aaae6d1ba45b1be25f
zhaozhu@luobotec.com <zhaozhu@luobotec.com>
2016-06-29 15:42:37 +0800

    first commit

---
+++ src/main/java/com/zhaozhu/study/App.java
@@ -0,0 +1,13 @@
+package com.zhaozhu.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
