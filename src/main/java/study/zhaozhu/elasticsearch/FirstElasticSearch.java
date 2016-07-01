7007bde4c03a4d4e3d096b1ff8af0db71b0628df
zhaozhu@luobotec.com <zhaozhu@luobotec.com>
2016-07-01 19:16:37 +0800

    添加ElasticSearch依赖

---
+++ src/main/java/study/zhaozhu/elasticsearch/FirstElasticSearch.java
@@ -0,0 +1,20 @@
+/**   
+* @Title: FirstElasticSearch.java 
+* @Description: TODO
+* @author zhaozhu@luobotec.com
+* @date 2016年7月1日 下午4:25:07 
+* @powered by 北京萝卜科技有限公司
+* @version V1.0   
+*/
+package study.zhaozhu.elasticsearch;
+
+/** 
+* @ClassName: FirstElasticSearch 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author zhaozhu@luobotec.com
+* @date 2016年7月1日 下午4:25:07 
+*  
+*/
+public class FirstElasticSearch {
+	
+}
