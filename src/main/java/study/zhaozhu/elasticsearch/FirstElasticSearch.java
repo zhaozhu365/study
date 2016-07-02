1900b55e45396825283ae93ed852d4d7926ec280
zhaozhu@luobotec.com <zhaozhu@luobotec.com>
2016-07-02 13:26:26 +0800

    modify

--- src/main/java/study/zhaozhu/elasticsearch/FirstElasticSearch.java
+++ src/main/java/study/zhaozhu/elasticsearch/FirstElasticSearch.java
@@ -8,13 +8,33 @@
 */
 package study.zhaozhu.elasticsearch;
 
-/** 
-* @ClassName: FirstElasticSearch 
-* @Description: TODO(这里用一句话描述这个类的作用) 
-* @author zhaozhu@luobotec.com
-* @date 2016年7月1日 下午4:25:07 
-*  
-*/
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
+/**
+ * @ClassName: FirstElasticSearch
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author zhaozhu@luobotec.com
+ * @date 2016年7月1日 下午4:25:07
+ * 
+ */
 public class FirstElasticSearch {
-	
+	public static void main(String[] args) {
+		
+//		Settings settings = Settings.settingsBuilder().put("cluster.name", "myClusterName").build();
+		
+		try {
+//			Client client = TransportClient.builder().settings(settings).build();
+			Client client = TransportClient.builder().build()
+					.addTransportAddress(new InetSocketTransportAddress(InetAddress.getLocalHost(), 9300));
+			System.out.println(client);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
