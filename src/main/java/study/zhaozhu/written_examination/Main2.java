1d14bfdc355a9bc47cd6dd3d2facc85d887ad1e2
zhaozhu@luobotec.com <zhaozhu@luobotec.com>
2016-08-10 17:12:02 +0800

    add

---
+++ src/main/java/study/zhaozhu/written_examination/Main2.java
@@ -0,0 +1,80 @@
+package study.zhaozhu.written_examination;
+
+import java.util.Scanner;
+
+/**
+ * stringbuilder 的insert和reverse方法
+ * @author Administrator
+ *
+ */
+public class Main2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		while (scan.hasNext()) {
+			String str1 = scan.next();
+			StringBuilder sb = null;
+			String str2 = scan.next();
+			int count = 0;
+			for (int i = 0; i < str1.length() + 1; i++) {
+				sb = new StringBuilder(str1);
+				String temp = sb.insert(i, str2).toString();
+				if (isHuiwen(temp)) {
+					count++;
+				}
+				sb = null;
+			}
+			System.out.println(count);
+		}
+
+	}
+
+	public static boolean isHuiwen(String input) {
+		if (input != null) {
+			StringBuilder sb = new StringBuilder(input);
+			sb.reverse();
+			String inputReverse = sb.toString();
+			if (input.equals(inputReverse)) {
+				return true;
+			}else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+//	public static boolean isHuiwen(String input) {
+//		if (input != null) {
+//			StringBuilder sb = new StringBuilder(input);
+//			sb.reverse();
+//			String inputReverse = sb.toString();
+////			for (int i = 0; i < input.length(); i++) {
+////				if (input.charAt(i) != inputReverse.charAt(i)) {
+////					return false;
+////				}
+////			}
+//			if (input.equals(inputReverse)) {
+//				return true;
+//			}else {
+//				return false;
+//			}
+//		} else {
+//			return false;
+//		}
+//	}
+//	public static boolean isHuiwen(String input) {
+//		if (input != null) {
+//			int length = input.length();
+//			for (int i = 0; i < length / 2; i++) {
+//				if (input.charAt(i) != input.charAt(length - i - 1)) {
+//					return false;
+//				}
+//			}
+//			return true;
+//		} else {
+//			return false;
+//		}
+//	}
+
+}
