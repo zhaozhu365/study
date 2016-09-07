81908f9e53b507daf349cbb1919677dbaa238e54
zhaozhu@luobotec.com <zhaozhu@luobotec.com>
2016-09-07 19:01:36 +0800

    test sumN2

--- src/main/java/study/zhaozhu/written_examination/didi/SumSubarray.java
+++ src/main/java/study/zhaozhu/written_examination/didi/SumSubarray.java
@@ -21,30 +21,64 @@ public class SumSubarray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// int[] arr = { 7,-1, 2, 1, 3 };
-		// System.out.println(sumN(arr));
+		 int[] arr = { 7,-1, 2, 1, 3, -2};
+		 System.out.println(sumN(arr));
+		 System.out.println(sumN2(arr));
 
-		Scanner scan = new Scanner(System.in);
-		while (scan.hasNext()) {
-			int n = scan.nextInt();
-			// 注意要对n做特殊处理,否则在new
-			// int[n]的时候会报错，java.lang.ArrayIndexOutOfBoundsException: -1
-			// 数组初始化的大小必须是n>=1，所以当n<1的时候，都要跳过
-			if (n <= 0) {
-				continue;
-			}
-
-			int[] array = new int[n];
-			for (int i = 0; i < n; i++) {
-				array[i] = scan.nextInt();
-			}
-			System.out.println(sumN(array));
-
-		}
+//		Scanner scan = new Scanner(System.in);
+//		while (scan.hasNext()) {
+//			int n = scan.nextInt();
+//			// 注意要对n做特殊处理,否则在new
+//			// int[n]的时候会报错，java.lang.ArrayIndexOutOfBoundsException: -1
+//			// 数组初始化的大小必须是n>=1，所以当n<1的时候，都要跳过
+//			if (n <= 0) {
+//				continue;
+//			}
+//
+//			int[] array = new int[n];
+//			for (int i = 0; i < n; i++) {
+//				array[i] = scan.nextInt();
+//			}
+//			System.out.println(sumN(array));
+//
+//		}
 
 	}
-
+	
 	/**
+	 * 3层循环的暴力方法
+	 * @param array
+	 * @return
+	 */
+	public static int sumN3(int [] array){
+		return 0;
+    }  
+	
+	/**
+	 * 2层循环的暴力破解法
+	 * @param array
+	 * @return
+	 */
+	public static int sumN2(int [] array){  
+        int sum=0,maxSum=Integer.MIN_VALUE;  
+        int n=array.length;  
+        int count=0;  
+        for(int i=0;i<n;i++){  
+            sum=0;  
+            for(int j=i;j<n;j++){  
+                sum+=array[j];  
+                if(sum>maxSum){  
+                    maxSum=sum;  
+                }  
+                count++;  
+            }  
+        }  
+        System.out.println("length="+n+"||count="+count);  
+        return maxSum;  
+    }  
+	
+	/**
+	 * 线性时间复杂度的方法
 	 * 注意要对n=1做特殊处理
 	 * 
 	 * @param array
