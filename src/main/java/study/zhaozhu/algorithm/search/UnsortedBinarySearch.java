b6ed38292ef6fef0ba5185f65cd26d13b881f67b
zhaozhu@luobotec.com <zhaozhu@luobotec.com>
2016-09-14 18:38:44 +0800

    1，无序数组二分查找思路提交 2，添加几道努比亚笔试题

--- src/main/java/study/zhaozhu/algorithm/search/UnsortedBinarySearch.java
+++ src/main/java/study/zhaozhu/algorithm/search/UnsortedBinarySearch.java
@@ -15,49 +15,14 @@ package study.zhaozhu.algorithm.search;
  */
 public class UnsortedBinarySearch {
 
+	// 无序数组的二分查找,并返回原始索引
+	// 定义两个数组，index，和B，index里存放索引，B里存放数据
+	// 每次找到一个最小的放到B里，同时把它的索引存在index中
+	// 然后对B二分查找，找到处于B中索引后，根据这个去index中找原始索引
+
 	static int[] a = new int[] { 1, 4, 9, 3, 24, 21, 6, 9, 9, 7, 6, 5, 3 };
 
 	public static void main(String[] args) {
-		int n = a.length,k = 2;
-		
-		System.out.println(unsorted_binarysearch(1,n,k));
-		
-	}
-
-	static int _sort(int left, int right) {
-		int x = a[left];
-		while (left < right) {
-			while (a[right] > x && left < right)
-				right--;
-			if (left < right) {
-				a[left] = a[right];
-				left++;
-			}
-			while (a[left] < x && left < right)
-				left++;
-			if (left < right) {
-				a[right] = a[left];
-				right--;
-			}
-		}
-		a[left] = x; // left==right
-		return left;
-	}
-
-	static int unsorted_binarysearch(int left, int right, int key) // 每次都选择起点作为“枢纽”
-	{
-		if (left > right)
-			return -1;
-
-		int x = a[left];
-		int mid_loc = _sort(left, right);
-
-		if (x == key)
-			return mid_loc;
-		else if (x > key)
-			return unsorted_binarysearch(left, mid_loc - 1, key);
-		else
-			return unsorted_binarysearch(mid_loc + 1, right, key);
 
 	}
 
--- src/main/java/study/zhaozhu/written_examination/lianhe30/ZidianxvQuanpailie.java
+++ src/main/java/study/zhaozhu/written_examination/lianhe30/ZidianxvQuanpailie.java
@@ -6,83 +6,59 @@
 */
 package study.zhaozhu.written_examination.lianhe30;
 
-/** 
-* @ClassName: ZidianxvQuanpailie 
-* @Description: TODO
-* @author zhaozhu
-* @date 2016年9月14日 上午10:11:35 
-*  
-*/
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * @ClassName: ZidianxvQuanpailie
+ * @Description: TODO
+ * @author zhaozhu
+ * @date 2016年9月14日 上午10:11:35
+ * 
+ */
 public class ZidianxvQuanpailie {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] a = new int[] { 1, 2, 3, 3, 4 };
+		func(a);
 
 	}
 	
-	private static void PermutationList()
-    {
-        int fromIndex, endIndex, changeIndex;
-        Sort(0, length - 1);
-        do
-        {
-            // 输出一种全排列
-            Output();
-            fromIndex = endIndex = length - 1;
-            // 向前查找第一个变小的元素
-            while (fromIndex > 0 && words[fromIndex] < words[fromIndex - 1]) --fromIndex;
-            changeIndex = fromIndex;
-            if (fromIndex == 0) break;
-            // 向后查找最后一个大于words[fromIndex-1]的元素
-            while (changeIndex + 1 < length && words[changeIndex + 1] > words[fromIndex - 1]) ++changeIndex;
-            Swap(fromIndex - 1, changeIndex);   // 交换两个值
-            InvertArray(fromIndex, endIndex);   // 对后面的所有值进行反向处理
-        } while (true);
-    }
+	/**
+	 * 根据题目要求，先将数组由小到大排序。然后输出第1个，
+	 * 输出第1个后，第1个从数组中删除。并将第1个缓存下来，输出第2个时，
+	 * 要和这个缓存下来的数进行比较，如果满足条件，才进行输出，否则判断出下一个。递归即可。
+	 * @param arr
+	 */
+	/**
+	 * 这个题还是有问题,比如，如果原数组为，2 3 3，这样就出问题了。得不到正确得结果
+	 * @param arr
+	 */
+	static void func(int[] arr) {
+		LinkedList<Integer> list = new LinkedList<>();
+		Arrays.sort(arr);
+		for (int i : arr) {
+			list.add(i);
+		}
+		int temp = list.get(0);
+		list.remove(0);
+		System.out.print(temp + " ");
+		while (list.size() > 0) {
+			for (int i = 0; i < list.size(); i++) {
+				if (list.get(i) - temp == 1) {
 
-	public ArrayList<String> Permutation(String str) {
-	         ArrayList<String> res = new ArrayList<String>();
-	        if (str==null||str.length()==0) {
-	            return res;
-	        }
-	        //将字符串转成字节数组，方便排序
-	        char [] a = str.toCharArray();
-	        //按字母表中的位置排序，如果忽略此步，会导致找到的全排列有所丢失。即给定字符串是bac，如果不重新排列成abc，那么abc-bac之间的字典序字符串会丢失
-	        Arrays.sort(a);
-	        while (true) {
-	            res.add(String.valueOf(a));
-	            int i = 0;
-	            //从右向左，寻找第一个比右邻小的元素，找到停止，负责一致扫描完整个数组
-	            for (i = a.length-2; i >=0&&a[i]>=a[i+1] ; i--);            
-	            int j = i;
-	
-	            //如果j==-1，也就是上面的for循环没找到符合的元素，说明现在的数组从左向右是递减的，即最后一个字典序，返回整个集合
-	            if (j == -1) {
-	                return res;
-	            }
-	
-	            //找到符合的元素j后，反向扫描，寻找比j元素大的中最小的一个。其实整个地方比较简单，我们在第一次for循环时，找到j，说明从n到j，即数组从右向左一直到j，是递增的，否则不会找到j。所以我们只需要从数组最右端先左扫描即可找到第一个比j大的，即使符合要求的。
-	            for (i = a.length-1; i>0&&a[i]<=a[j]; i--);         
-	            int k = i;
-	
-	            //找到j和k元素后，交换两个元素，
-	            char b = a[j];
-	            a[j] = a[k];
-	            a[k] = b;
-	
-	            //上面的工作我们只是找到了j位置的元素，j+1到n位置的元素还是一个递增的，不符合字典序，下面是逆序的过程
-	            for (int l = j+1; l < (a.length+j)/2+1; l++) {
-	                char c = a[l];
-	                a[l] = a[a.length-l+j];
-	                a[a.length-l+j] = c;
-	            }
-	
-	            //全部处理完，找到所需要的字典序，在找到的字典序数组基础上，再去找下一个字典序
-	        }
-	    }
+				} else {
+					System.out.print(list.get(i) + " ");
+					temp = list.get(i);
+					list.remove(i);
+					break;
+				}
+			}
+		}
 
-	
+	}
+
 }
